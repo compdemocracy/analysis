485a493b4877f9b1eaa1a57ca3c69a88bf892682
Christopher Small <metasoarous@gmail.com>
2020-07-31 16:27:53 -0700

    more literate notebook rewrites

--- notebooks/oz/umap-test.clj
+++ notebooks/oz/umap-test.clj
@@ -15,25 +15,28 @@
 
 ;; Note! This document is currently written in terms of the next iteration of the CLJ -> Hiccup mechanism
 
-;; # UMAP Test!
+[:h1 "UMAP Test!"]
 
-;; This is a basic test of **UMAP** for dimensionality reduction!
+[:md
+ "This is a basic test of **UMAP** for dimensionality reduction!
+
+ First we're going to load up the iris dataset from sklearn.
+ "]
  
-;; First we're going to load up the data from sklearn.
-
 (def iris (sk-data/load_iris))
 iris
 (py/att-type-map iris)
-(py.- iris DESCR)
+;(py.- iris DESCR)
+
+[:pre (py.- iris DESCR)]
 
 
-;; Next we're going to load up the iris-df
+[:md
+ "Wow! That's quite a lot :-D.
+  Next we're going to translate this into a pandas dataframe:"]
 
 (def iris-df (pandas/DataFrame (py.- iris data) :columns (py.- iris feature_names)))
 
-;; Is this how you display something as data?
-[:pprint
- (py/att-type-map iris-df)]
 
 (def iris-name-series
   (let [iris-name-map (zipmap (range 3)
@@ -44,8 +47,8 @@ iris
 
 (py. iris-df __setitem__ "species" iris-name-series)
 
-[:print
- (py/get-item iris-df "species")]
+;[:print
+ ;(py/get-item iris-df "species")]
 
 (defn to-dicts [df]
   "Get dict objects out of df, and convert to jvm seq of maps for json export"
@@ -54,7 +57,7 @@ iris
 ;; Playing around with getting a specific item
 (py/get-item iris-df "species")
 
-;; Here's what our data looks like
+[:md "Here's what our data looks like"]
 
 [:vega-lite
  {:data {:values (to-dicts iris-df)}
@@ -69,8 +72,7 @@ iris
              :color {:field "species"}
              :shape {:field "species"}}}]
 
-
-;; Now time to run UMAP!
+[:md "Now time to run UMAP!"]
 
 (def matrix
   (py/as-numpy
@@ -86,9 +88,9 @@ iris
 
 (def embedding (py. reducer fit_transform matrix))
 
-;(py.- embedding shape)
+(py.- embedding shape)
 
-;(str (first embedding))
+(str (first embedding))
 
 ;; Here's what that looks like!
 
@@ -101,36 +103,3 @@ iris
                  :scale {:zero false}}
              :y {:field "1"
                  :scale {:zero false}}}}]
-
-;; Currently, only the final form will be shown, so recreating in terms of current Oz support for displaying
-;; the final form.
-;; Eventually Oz will support rendering top-level `;;` comments as markdown, and will also render top-level
-;; vector literals as hiccup, as implied here and in the comments and code blocks above.
-
-;; Will want options to turn on/off showing the source code, marginalia style docs, etc.
-
-[:div
-  [:md "Here's what our data looks like"]
-  [:vega-lite
-   {:data {:values (to-dicts iris-df)}
-    :mark {:type :point :tooltip {:content :data}}
-    :width 500
-    ;:encoding {:x {:field "sepal-length"}
-               ;:y {:field "sepal-width"}}}
-    :encoding {:x {:field "sepal length (cm)"
-                   :scale {:zero false}}
-               :y {:field "sepal width (cm)"
-                   :scale {:zero false}}
-               :color {:field "species"}
-               :shape {:field "species"}}}]
-  [:md "Here's the UMAP projection!"]
-  [:vega-lite
-   {:data {:values (mapv vec embedding)}
-    :mark :point
-    :width 800
-    :height 400
-    :encoding {:x {:field "0"
-                   :scale {:zero false}}
-               :y {:field "1"
-                   :scale {:zero false}}}}]]
-
