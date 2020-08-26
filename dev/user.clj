83f0719c83d35d7044e2af6f39ddeae5fa452c63
Christopher Small <metasoarous@gmail.com>
2020-08-26 14:10:12 -0700

    tweak dev/user.clj

--- dev/user.clj
+++ dev/user.clj
@@ -1,17 +1,12 @@
 (ns user
   (:require [tech.ml.dataset :as ds]
             [tech.v2.datatype :as dt]
+            [polis.math :as math]
             [libpython-clj.require :refer [require-python]]
             [libpython-clj.python :as py :refer [py. py.. py.-]]
             [oz.core :as oz]))
 
-
-(require-python '[sklearn.datasets :as sk-data]
-                '[sklearn.model_selection :as sk-model]
-                '[numpy :as numpy]
-                '[numba :as numba]
-                '[pandas :as pandas]
-                '[umap :as umap])
+(oz/start-server! 3860)
 
 
 (comment
@@ -19,7 +14,9 @@
   (oz/view! {:data {:values [{:a 1 :b 4} {:a 2 :b 2}]}
              :mark :point
              :encoding {:x {:field :a}
-                        :y {:field :b}}})
+                        :y {:field :b}}}
+            :port 3860)
+
   (oz/build! [{:from "notebooks/oz"
                :to "notebooks/build"}
               {:from "local/notebooks"
