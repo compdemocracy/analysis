ec9caa19a38f1bc258951499494b41900410303d
Christopher Small <metasoarous@gmail.com>
2020-09-17 15:18:05 -0700

    Add a simpler umap distance metric
    This one ignores both passes and missing data, and only accounts for distance with respect to comments both participants voted on

--- umap_metric.py
+++ umap_metric.py
@@ -6,3 +6,8 @@ def sparsity_aware_dist(a, b):
     n_both_seen = len(a) - (np.isnan(a) | np.isnan(b)).sum()
     return (n_both_seen - (a == b).sum() + 1) / (n_both_seen + 2)
 
+@numba.njit()
+def sparsity_aware_dist2(a, b):
+    definitive_votes = (a != 0) | (b != 0)
+    n_both_seen = definitive_votes.sum()
+    return (n_both_seen - (definitive_votes & (a == b)).sum() + 1) / (n_both_seen + 1)
