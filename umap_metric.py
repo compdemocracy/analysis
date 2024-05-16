7da3c452b96fb7a2da8378720662c0cadd1b8fcf
Bennie Rosas <bennie.rosas@blvd.co>
2024-05-15 23:35:11 -0500

    update umap_metric

--- umap_metric.py
+++ umap_metric.py
@@ -11,3 +11,4 @@ def sparsity_aware_dist2(a, b):
     definitive_votes = (a != 0) | (b != 0)
     n_both_seen = definitive_votes.sum()
     return (n_both_seen - (definitive_votes & (a == b)).sum() + 1) / (n_both_seen + 1)
+
